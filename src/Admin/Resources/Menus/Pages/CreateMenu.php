023accacf5542eb6246df36e8f4caff04d5ebee1
maxboyko <maxboiko@divotek.com>
2025-10-01 16:14:37 +0300

    fix create menu page layout

--- src/Admin/Resources/Menus/Pages/CreateMenu.php
+++ src/Admin/Resources/Menus/Pages/CreateMenu.php
@@ -4,8 +4,36 @@ namespace SmartCms\Menu\Admin\Resources\Menus\Pages;
 
 use Filament\Resources\Pages\CreateRecord;
 use SmartCms\Menu\Admin\Resources\Menus\MenuResource;
+use SmartCms\Menu\MenuPlugin;
+use SmartCms\Support\Admin\Components\Actions\SaveAction;
+use SmartCms\Support\Admin\Components\Actions\SaveAndClose;
 
 class CreateMenu extends CreateRecord
 {
     protected static string $resource = MenuResource::class;
+
+    public function getSubNavigation(): array
+    {
+        $additionalItems = [];
+        if (MenuPlugin::$cluster) {
+            foreach (MenuPlugin::$cluster::getClusteredComponents() as $component) {
+                $additionalItems = array_merge($additionalItems, $component::getNavigationItems());
+            }
+        }
+
+        return array_merge(parent::getSubNavigation(), $additionalItems);
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            \Filament\Actions\ActionGroup::make([
+                SaveAction::make($this),
+            ])->link()->label('Actions')
+                ->icon(\Filament\Support\Icons\Heroicon::ChevronDown)
+                ->size(\Filament\Support\Enums\Size::Small)
+                ->iconPosition(\Filament\Support\Enums\IconPosition::After)
+                ->color('primary'),
+        ];
+    }
 }
