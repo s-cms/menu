ac096fc27c1dc9e1f2ab5b47fdb5c0fd47d6bff4
TsukuroTadzaki <dstoianov87@gmail.com>
2026-04-05 03:39:35 +0300

    feat(i18n): add UK, DE, PL translations for admin

---
+++ resources/lang/de/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'type' => 'Typ',
+    'link' => 'Link',
+    'title' => 'Titel',
+    'open_in_new_tab' => 'In neuem Tab öffnen',
+    'url' => 'URL',
+    'menu' => 'Menü',
+    'items_count' => 'Anzahl der Elemente',
+    'nested_menu' => 'Untermenü',
+    'select_menu' => 'Menü auswählen',
+];
